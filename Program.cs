8fad5ac8293a4ceba75beae2368cd8264c1ba94a
mustafaberk01 <mustafaberkay5234@gmail.com>
2023-01-21 12:01:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+public class ReverseEachWordChallange
+{
+    static string ReverseEachWord(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        string[] words = input.Split(' ');
+        StringBuilder stringToReturn = new StringBuilder(input.Length);
+
+        foreach (var word in words)
+        {
+            stringToReturn.Append(Reverse(word) + " ");
+        }
+        return stringToReturn.ToString();
+    }
+
+    static String Reverse(String input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+
+        char[] arr = input.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string with no punctuation (or 'exit' to close the console)");
+        string userInput = Console.ReadLine();
+        while(userInput != "exit")
+        {
+            Console.WriteLine(ReverseEachWord(userInput));
+            Console.WriteLine("Enter a string with no punctuation (or 'exit' to close the console)");
+            userInput = Console.ReadLine();
+        }
+    }
+}
